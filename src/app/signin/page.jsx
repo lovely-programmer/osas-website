06ee6a045f351978a3db6b5ded4ee77a281027ab
lovely-programmer <olaoluwaolasunkanmi5@gmail.com>
2024-01-04 05:35:59 +0100

    new update

--- src/app/signin/page.jsx
+++ src/app/signin/page.jsx
@@ -5,10 +5,23 @@ import { MultiplePageContext } from "../../context/MultiplePageContext";
 import GoogleSignin from "../../components/registration/GoogleSignin";
 import Registration from "../../components/registration/Registration";
 import Post from "../../components/registration/Post";
+import { useSession } from "next-auth/react";
+import { getAUser } from "../../requests/requests";
+import { useRouter } from "next/navigation";
+import Spinner from "../../components/spinner/Spinner";
 
 export default function Signin() {
   const { multiplePage, currentIndex, next, step } =
     useContext(MultiplePageContext);
+
+  const { user, isLoading } = getAUser();
+  const router = useRouter();
+
+  if (user?.createdSuccessfully) router.push("/");
+
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   useEffect(() => {
     multiplePage([
