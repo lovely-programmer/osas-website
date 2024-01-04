ad6f29e5957c1b9961a4c6c027ffd7ccf6c4cdcb
lovely-programmer <olaoluwaolasunkanmi5@gmail.com>
2024-01-04 04:53:35 +0100

    nextauth

--- src/utils/NextAuth.jsx
+++ src/utils/NextAuth.jsx
@@ -2,13 +2,16 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { getAUser } from "../requests/requests";
+import Spinner from "../components/spinner/Spinner";
 
 export default function NextAuth({ children }) {
   const { status } = useSession();
-  const { user } = getAUser();
+  const { user, isLoading } = getAUser();
   const router = useRouter();
 
-  console.log(user?.createdSuccessfully);
+  if (isLoading) {
+    return <Spinner />;
+  }
 
   if (status === "unauthenticated" && user?.createdSuccessfully == false) {
     router.push("/signin");
