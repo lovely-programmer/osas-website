b7d0cf7098c276718c801a1b25d19dba4dcafe11
lovely-programmer <olaoluwaolasunkanmi5@gmail.com>
2024-01-22 06:50:42 +0100

    verce

--- src/requests/requests.js
+++ src/requests/requests.js
@@ -3,7 +3,7 @@ import useSWR from "swr";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-const url = "https://studenthelpers.netlify.app";
+const url = "https://osas-website.vercel.app";
 
 // "https://studenthelpers.netlify.app"
 // http://localhost:3000
