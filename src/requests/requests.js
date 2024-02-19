aa7de80ab53d9c3c82786109b977ecd59f2c210c
lovely-programmer <olaoluwaolasunkanmi5@gmail.com>
2024-02-19 03:42:49 +0100

    render

--- src/requests/requests.js
+++ src/requests/requests.js
@@ -3,12 +3,13 @@ import useSWR from "swr";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-const url = "https://www.studentcentral.online";
+const url = "https://student-support.onrender.com";
 
 // "https://studenthelpers.netlify.app"
 // http://localhost:3000
 // https://osas-website.vercel.app
 // https://www.studentcentral.online
+// https://student-support.onrender.com
 
 export const getUser = (email) => {
   const { data, error, isLoading } = useSWR(
