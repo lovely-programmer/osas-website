80d8cd21745dc79919bd0506a37ca041c51ff7ec
lovely-programmer <olaoluwaolasunkanmi5@gmail.com>
2024-03-07 09:10:11 +0100

    update

--- src/utils/Navbar.jsx
+++ src/utils/Navbar.jsx
@@ -1,10 +1,22 @@
 "use client";
 import { usePathname } from "next/navigation";
 import Navbar from "../components/navbar/Navbar";
+import WelcomeNav from "../components/navbar/welcomeNav";
+import { useEffect, useState } from "react";
 
 export default function ShowNavbar() {
   const pathname = usePathname();
-  if (pathname !== "/message" || pathname !== "/welcome") {
+  const [messagePage, setMessagePage] = useState(false);
+
+  useEffect(() => {
+    if (pathname === "/message") setMessagePage(true);
+  }, [pathname]);
+
+  if (pathname === "/welcome") {
+    return <WelcomeNav />;
+  }
+
+  if (!messagePage) {
     return <Navbar />;
   }
 }
