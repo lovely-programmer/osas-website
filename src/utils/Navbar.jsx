643b5d93993a273f9b0b76cb6c97109e67e78c2d
lovely-programmer <olaoluwaolasunkanmi5@gmail.com>
2024-03-07 08:05:01 +0100

    checking

--- src/utils/Navbar.jsx
+++ src/utils/Navbar.jsx
@@ -4,7 +4,7 @@ import Navbar from "../components/navbar/Navbar";
 
 export default function ShowNavbar() {
   const pathname = usePathname();
-  if (pathname !== "/message" && pathname !== "/welcome") {
+  if (pathname !== "/message" || pathname !== "/welcome") {
     return <Navbar />;
   }
 }
