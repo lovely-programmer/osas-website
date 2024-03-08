15198a7f42da1e0bb9b0aa240fc7d5d89b38d561
lovely-programmer <olaoluwaolasunkanmi5@gmail.com>
2024-03-08 08:49:00 +0100

    update-nav

--- src/utils/Navbar.jsx
+++ src/utils/Navbar.jsx
@@ -8,8 +8,14 @@ export default function ShowNavbar() {
   const pathname = usePathname();
   const [showNav, setShowNav] = useState(true);
 
+  console.log(showNav);
+
   useEffect(() => {
-    if (pathname === "/message" || pathname === "/welcome") {
+    if (
+      pathname === "/message" ||
+      pathname === "/welcome" ||
+      pathname === "/siginin"
+    ) {
       setShowNav(false);
     } else {
       setShowNav(true);
@@ -24,3 +30,10 @@ export default function ShowNavbar() {
     return <Navbar />;
   }
 }
+
+//  pathname === "/posts/freeuseditems" ||
+//       pathname === "/posts/skills" ||
+//       pathname === "/posts/studentMarket" ||
+//       pathname === "/posts/rent" ||
+//       pathname === "/posts/profile" ||
+//       pathname === "/posts/skills" ||
