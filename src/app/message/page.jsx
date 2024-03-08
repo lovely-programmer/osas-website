5b92810f7152135a57505cba3b26177c4fb3ef89
lovely-programmer <olaoluwaolasunkanmi5@gmail.com>
2024-03-08 18:49:12 +0100

    text

--- src/app/message/page.jsx
+++ src/app/message/page.jsx
@@ -324,7 +324,7 @@ export default function Message() {
                   onChange={(e) => setText(e.target.value)}
                   type="text"
                   autoFocus
-                  value={text}
+                  // value={text}
                 />
                 <label htmlFor="photoImg">
                   <IoMdAttach style={{ cursor: "pointer" }} />
@@ -482,7 +482,7 @@ export default function Message() {
                   onChange={(e) => setText(e.target.value)}
                   type="text"
                   autoFocus
-                  value={text}
+                  // value={text}
                 />
                 <label htmlFor="photoImg">
                   <IoMdAttach style={{ cursor: "pointer" }} />
