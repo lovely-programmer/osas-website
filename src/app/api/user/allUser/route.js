e35ae7bf4b8dd1b57573622f9e86e6cab9b97566
lovely-programmer <olaoluwaolasunkanmi5@gmail.com>
2024-04-22 09:09:05 +0100

    small update

--- src/app/api/user/allUser/route.js
+++ src/app/api/user/allUser/route.js
@@ -3,7 +3,9 @@ import prisma from "../../../../utils/connect";
 
 export const GET = async (req) => {
   try {
-    const user = await prisma.user.findMany();
+    const user = await prisma.user.findMany({
+      orderBy: [{ email: "desc" }],
+    });
     return new NextResponse(JSON.stringify(user), { status: 200 });
   } catch (err) {
     return new NextResponse(
