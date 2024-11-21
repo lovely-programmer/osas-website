13269949b0fd6d1e1265fb27fd07f6985f4997de
lovely-programmer <olaoluwaolasunkanmi5@gmail.com>
2024-11-21 06:36:47 +0100

    registration

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -26,6 +26,8 @@ export default function Home() {
 
   if (user?.country == null && user?.createdSuccessfully == false) {
     router.push("/signup/welcome");
+  } else if (user?.createdSuccessfully == false) {
+    router.push("/signup/post");
   }
 
   if (isLoading) return <Spinner />;
